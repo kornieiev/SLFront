d80c8227d55d4c32eca04aa912a167b6ccfcf2e2
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-02 13:05:18 +0200

    add BrowserRouter and routes

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import AboutPage from "./AboutPage";
+import HomePage from "./HomePage";
+import NotFoundPage from "./NotFoundPage";
+import ProductsPage from "./ProductsPage";
+
+export default {
+  HomePage,
+  AboutPage,
+  ProductsPage,
+  NotFoundPage,
+};
