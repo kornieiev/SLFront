a399adbcaaefd1557ce7052699db86577a9b5d5e
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-16 13:47:59 +0200

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/slfront",
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+    },
+  },
   server: {
     proxy: {
       "/api": {
