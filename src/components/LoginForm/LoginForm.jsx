9a39da3d51f595e853f0ebbee77d8730cecf437f
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-10-21 15:46:09 +0200

    fix error msg when invalid login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,3 @@
-import toast, { Toaster } from "react-hot-toast";
-
 import { useDispatch, useSelector } from "react-redux";
 import { logIn } from "../../redux/auth/operations";
 import css from "./LoginForm.module.css";
@@ -8,8 +6,6 @@ import { error } from "../../redux/auth/selectors";
 export const LoginForm = () => {
   const errorMessage = useSelector(error);
 
-  const notify = () => toast.error("Incorrect login or password");
-
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
@@ -17,10 +13,6 @@ export const LoginForm = () => {
     const form = e.currentTarget;
     const email = form.elements.email.value.trim().replace(/\s+/g, "");
     const password = form.elements.password.value.trim().replace(/\s+/g, "");
-
-    if (errorMessage) {
-      notify();
-    }
 
     dispatch(
       logIn({
@@ -33,19 +25,26 @@ export const LoginForm = () => {
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete='off'>
-      <label className={css.label}>
-        Email:
-        <input className={css.input} type='email' name='email' required />
-      </label>
-      <label className={css.label}>
-        Password:
-        <input className={css.input} type='password' name='password' />
-      </label>
-      <button className={css.btn} type='submit'>
-        Log In
-      </button>
-      <Toaster position='top-right' reverseOrder={false} />
-    </form>
+    <>
+      <form className={css.form} onSubmit={handleSubmit} autoComplete='off'>
+        <label className={css.label}>
+          Email:
+          <input className={css.input} type='email' name='email' required />
+        </label>
+        <label className={css.label}>
+          Password:
+          <input className={css.input} type='password' name='password' />
+        </label>
+        <button className={css.btn} type='submit'>
+          Log In
+        </button>
+      </form>
+      {errorMessage && (
+        <>
+          <p className={css.errorMsg}>Login Failed!</p>
+          <p className={css.errorMsg}>Please enter valid data</p>
+        </>
+      )}
+    </>
   );
 };
